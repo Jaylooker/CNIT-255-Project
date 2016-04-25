dca5d98e8b901a9ec3bef23be80c3f90b8224847
Jaylooker <jaylookerwooga@gmail.com>
2016-04-25 01:35:18 -0400

    Added Audio
    Component of player.

--- Assets/Resources/Scripts/Classes/Menu.cs
+++ Assets/Resources/Scripts/Classes/Menu.cs
@@ -5,10 +5,17 @@ using System.Collections;
 public class Menu : MonoBehaviour {
 
     private GameObject player;
+    private AudioSource audiosource;
     private AudioClip gamemusic1;
     private int DemoScene;
     private int MenuScene;
 
+    void Awake()
+    {
+        audiosource = gameObject.AddComponent<AudioSource>();
+        gamemusic1 = Resources.Load("Assets/Audio/song1") as AudioClip;
+        audiosource.clip = gamemusic1;
+    }
     void Start()
     {
         MenuScene = 0;
@@ -18,6 +25,9 @@ public class Menu : MonoBehaviour {
     public void Play()
     {
         SceneManager.LoadScene(DemoScene);
+  
+        audiosource.Play();
+        
     }
 	public void Quit()
     {
